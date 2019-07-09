c8b5e5168d018d795562235ef9f56144000012d6
Aaron Stewart <me@aaronstewart.io>
2019-07-09 12:17:33 -0400

    setting up hackernews api fetch

--- app/index.js
+++ app/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const JSON_QUERY = ".json?print=pretty";
+
 class App extends React.Component{
+
+    async componentDidMount() {
+        const response = await fetch(`https://hacker-news.firebaseio.com/v0/topstories${JSON_QUERY}`);
+        const json = await response.json();
+        console.log(json);
+    }
+
     render(){
         return(
             <div>Hello World</div>
